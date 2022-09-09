9178ac04d8a096af8c7ea1debb1399af61833138
ajruffin1 <112420495+ajruffin1@users.noreply.github.com>
2022-09-09 15:41:22 -0400

    Update StudentService.js

--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const STUDENT_BASE_REST_API_URL = 'https://studentsbackendmain.herokuapp.com';
+const STUDENT_BASE_REST_API_URL = 'https://studentsbackendmain.herokuapp.com/';
 //const STUDENT_BASE_REST_API = 'http://localhost:8080/studentsCreate';
 
 class StudentService{
